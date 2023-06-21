e118892ddf54159b97d2734a3fa7803d23ff1d9d
dMuguti <omiahderek56@gmail.com>
2023-06-21 21:54:56 +0300

    Done

--- client/src/components/MovieForm.js
+++ client/src/components/MovieForm.js
@@ -14,17 +14,24 @@ function MovieForm() {
     female_director: false,
   });
 
-  function handleSubmit(e) {
+  const [errors, setErrors] = useState([]);
+
+  async function handleSubmit(e) {
     e.preventDefault();
-    fetch("/movies", {
+    const response = await fetch("/movies", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(formData),
-    })
-      .then((response) => response.json())
-      .then((newMovie) => console.log(newMovie));
+    });
+    const data = await response.json();
+
+    if (response.ok) {
+      console.log("Movie created:", data);
+    } else {
+      setErrors(data.errors);
+    }
   }
 
   function handleChange(e) {
@@ -39,6 +46,13 @@ function MovieForm() {
   return (
     <Wrapper>
       <form onSubmit={handleSubmit}>
+        {errors.length > 0 && (
+          <ul style={{ color: "red" }}>
+            {errors.map((error, index) => (
+              <li key={index}>{error}</li>
+            ))}
+          </ul>
+        )}
         <FormGroup>
           <label htmlFor="title">Title</label>
           <input
